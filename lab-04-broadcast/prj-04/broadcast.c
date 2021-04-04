3faeb3afeb436d8f498f1326a21d4cf92d29dedc
Xuzhou Zheng <zhengxuzhou18@mails.ucas.edu.cn>
2021-04-04 18:30:19 +0800

    lab 04: code finished (in broadcast.c)

--- lab-04-broadcast/prj-04/broadcast.c
+++ lab-04-broadcast/prj-04/broadcast.c
@@ -5,10 +5,14 @@ extern ustack_t *instance;
 
 void broadcast_packet(iface_info_t *iface, const char *packet, int len)
 {
-    // TODO: broadcast packet
-    fprintf(stdout, "TODO: broadcast packet.\n");
-    while (!list_empty(iface->list))
+    // OK TODO: broadcast packet
+    // fprintf(stdout, "TODO: broadcast packet.\n");
+    iface_info_t *iface_node;
+    list_for_each_entry(iface_node, &instance->iface_list, list)
     {
-        ;
+        if (iface_node != iface)
+        {
+            iface_send_packet(iface_node, packet, len);
+        }
     }
 }
--- lab-04-broadcast/prj-04/include/list.h
+++ lab-04-broadcast/prj-04/include/list.h
@@ -11,25 +11,32 @@ struct list_head
 #define list_empty(list) ((list)->next == (list))
 
 #define list_entry(ptr, type, member) \
-    (type *)((char *)ptr - offsetof(type, member))
+    (type *)((char *)ptr -            \
+             offsetof(type, member))
 
-#define list_for_each_entry(pos, head, member)                 \
-    for (pos = list_entry((head)->next, typeof(*pos), member); \
-         &pos->member != (head);                               \
-         pos = list_entry(pos->member.next, typeof(*pos), member))
+#define list_for_each_entry(pos, head, member)   \
+    for (pos = list_entry((head)->next,          \
+                          typeof(*pos), member); \
+         &pos->member != (head);                 \
+         pos = list_entry(pos->member.next,      \
+                          typeof(*pos), member))
 
-#define list_for_each_entry_safe(pos, q, head, member)          \
-    for (pos = list_entry((head)->next, typeof(*pos), member),  \
-        q = list_entry(pos->member.next, typeof(*pos), member); \
-         &pos->member != (head);                                \
-         pos = q, q = list_entry(pos->member.next, typeof(*q), member))
+#define list_for_each_entry_safe(pos, q, head, member) \
+    for (pos = list_entry((head)->next,                \
+                          typeof(*pos), member),       \
+        q = list_entry(pos->member.next,               \
+                       typeof(*pos), member);          \
+         &pos->member != (head);                       \
+         pos = q, q = list_entry(pos->member.next,     \
+                                 typeof(*q), member))
 
 static inline void init_list_head(struct list_head *list)
 {
     list->next = list->prev = list;
 }
 
-static inline void list_insert(struct list_head *new, struct list_head *prev,
+static inline void list_insert(struct list_head *new,
+                               struct list_head *prev,
                                struct list_head *next)
 {
     next->prev = new;

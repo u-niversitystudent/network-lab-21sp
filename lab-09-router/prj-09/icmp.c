8596e134e550955ad7b99148c25e51d63fd30df0
Xuzhou Zheng <zhengxuzhou18@mails.ucas.edu.cn>
2021-05-06 08:48:42 +0800

    lab 09: init

---
+++ lab-09-router/prj-09/icmp.c
@@ -0,0 +1,13 @@
+#include "icmp.h"
+#include "arp.h"
+#include "base.h"
+#include "ip.h"
+#include "rtable.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// send icmp packet
+void icmp_send_packet(const char *in_pkt, int len, u8 type, u8 code) {
+  fprintf(stderr, "TODO: malloc and send icmp packet.\n");
+}

6ea468552b011a1908465d6c741ab3c326ed0814
Xuzhou Zheng <zhengxuzhou18@mails.ucas.edu.cn>
2021-05-15 10:09:17 +0800

    lab 10: init

---
+++ lab-10-lookup/prj-10/include/prefix_tree.h
@@ -0,0 +1,12 @@
+//
+// Created by zheng on 5/14/2021.
+//
+
+#ifndef PRJ_10_PREFIX_TREE_H
+#define PRJ_10_PREFIX_TREE_H
+
+typedef struct pt_node{
+    pt_node_t *lchild, *rchild;
+}pt_node_t;
+
+#endif //PRJ_10_PREFIX_TREE_H
---
+++ lab-10-lookup/prj-10/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void prefix_tree(){
+    construct_tree();
+
+}
+
+int main() {
+
+    prefix_tree();
+
+    return 0;
+}
